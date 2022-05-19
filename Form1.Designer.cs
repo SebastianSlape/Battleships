fce001da1fbf5a78a2749e046adb0e971cc4b9b4
SebastianSlape <68803447+SebastianSlape@users.noreply.github.com>
2022-05-20 08:43:43 +0930

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,7 +29,9 @@
         private void InitializeComponent()
         {
             this.dgvPlace = new System.Windows.Forms.DataGridView();
-            this.btnPlaceShips = new System.Windows.Forms.Button();
+            this.btnPlaceShip = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPlace)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,6 +46,7 @@
             this.dgvPlace.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvPlace.Location = new System.Drawing.Point(232, 49);
             this.dgvPlace.Name = "dgvPlace";
+            this.dgvPlace.ReadOnly = true;
             this.dgvPlace.RowHeadersWidth = 51;
             this.dgvPlace.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
             this.dgvPlace.RowTemplate.Height = 29;
@@ -51,22 +54,44 @@
             this.dgvPlace.Size = new System.Drawing.Size(516, 348);
             this.dgvPlace.TabIndex = 0;
             // 
-            // btnPlaceShips
+            // btnPlaceShip
             // 
-            this.btnPlaceShips.Location = new System.Drawing.Point(34, 49);
-            this.btnPlaceShips.Name = "btnPlaceShips";
-            this.btnPlaceShips.Size = new System.Drawing.Size(151, 54);
-            this.btnPlaceShips.TabIndex = 1;
-            this.btnPlaceShips.Text = "Place Ships";
-            this.btnPlaceShips.UseVisualStyleBackColor = true;
-            this.btnPlaceShips.Click += new System.EventHandler(this.btnPlaceShips_Click);
+            this.btnPlaceShip.Location = new System.Drawing.Point(34, 49);
+            this.btnPlaceShip.Name = "btnPlaceShip";
+            this.btnPlaceShip.Size = new System.Drawing.Size(151, 54);
+            this.btnPlaceShip.TabIndex = 1;
+            this.btnPlaceShip.Text = "Place Ships";
+            this.btnPlaceShip.UseVisualStyleBackColor = true;
+            this.btnPlaceShip.Click += new System.EventHandler(this.btnPlaceShip_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(34, 128);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(151, 54);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear Fleet";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(34, 343);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(151, 54);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start Game";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnPlaceShips);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnPlaceShip);
             this.Controls.Add(this.dgvPlace);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -79,6 +104,8 @@
         #endregion
 
         private DataGridView dgvPlace;
-        private Button btnPlaceShips;
+        private Button btnPlaceShip;
+        private Button btnClear;
+        private Button btnStart;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -10,20 +10,42 @@ namespace Battleships
         WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
         Utility utility = new Utility();
         Random rnd = new Random();
+
         bool noahMode = false;
         string soundFolder = "";
+
         string[] soundArray = new string[2] { "lessgo.mp3", "yeahbaby.mp3" };
+        
+        string[] shipNames = new string[5] { "Carrier", "Battleship", "Cruiser", "Submarine", "Destroyer" };
+        int[] shipSize = new int[5] { 5, 4, 3, 3, 2 };
+        bool[] shipsPlaced = new bool[5] { false, false, false, false, false };
 
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
+        public void PlayBadSound()
         {
             // Initialize Location Of Folders
             soundFolder = utility.NavigateParents(Environment.CurrentDirectory, 3) + "\\Sounds";
 
+            wplayer.controls.stop();
+            wplayer.URL = soundFolder + "\\" + soundArray[rnd.Next(0, soundArray.Length)];
+            wplayer.controls.play();
+        }
+
+        public void PlayGoodSound()
+        {
+            // Initialize Location Of Folders
+            soundFolder = utility.NavigateParents(Environment.CurrentDirectory, 3) + "\\Sounds";
+
+            wplayer.controls.stop();
+            wplayer.URL = soundFolder + "\\" + soundArray[rnd.Next(0, soundArray.Length)];
+            wplayer.controls.play();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
             string name = string.Join(' ', Regex.Split(utility.GetNameOfUser(), @"(?<!^)(?=[A-Z])")); // Regex to Seperate Name by Capitalization ("JohnDoe" => "John Doe")
             
@@ -53,13 +75,112 @@ namespace Battleships
                 dgvPlace.Rows[rowHeader].HeaderCell.Value = letters[rowHeader].ToString();
             }
         }
-        private void btnPlaceShips_Click(object sender, EventArgs e)
-        {
-            Interaction.MsgBox(soundFolder);
+        private void btnPlaceShip_Click(object sender, EventArgs e)
+        {   
+            int columnNum = dgvPlace.SelectedCells[0].ColumnIndex;
+            int rowNum = dgvPlace.SelectedCells[0].RowIndex;
+            bool columnValid = true;
+            bool rowValid = true;
 
-            wplayer.controls.stop();
-            wplayer.URL = soundFolder + "\\" + soundArray[rnd.Next(0,2)];
-            wplayer.controls.play();
+            for (int i = 0; i < dgvPlace.SelectedCells.Count; i++)
+            {
+                if (dgvPlace.SelectedCells[i].ColumnIndex != columnNum)
+                {
+                    columnValid = false;
+                }
+                if (dgvPlace.SelectedCells[i].RowIndex != rowNum)
+                {
+                    rowValid = false;
+                }
+            }
+            if (!columnValid && !rowValid)
+            {
+                Interaction.MsgBox("Ship Invalid. Too Wide.");
+                return;
+            }
+            int count = dgvPlace.SelectedCells.Count;
+            if (count <= 1)
+            {
+                Interaction.MsgBox("Ship Invalid. Not Enough Cells Selected.");
+                return;
+            } 
+            else if (count >= 6)
+            {
+                Interaction.MsgBox("Ship Invalid. Too Many Cells Selected.");
+                return;
+            }
+
+            for (int j = 0; j < count; j++)
+            {
+                if (dgvPlace.SelectedCells[j].Style.BackColor == Color.Red)
+                {
+                    Interaction.MsgBox("You already placed a ship there");
+                    return;
+                }
+            }
+
+            for (int i = 0; i < shipSize.Length; i++)
+            {
+                if (count == shipSize[i] && count != 3)
+                {
+                    if (shipsPlaced[i] == true)
+                    {
+                        Interaction.MsgBox("You already placed a " + shipNames[i]);
+                        return;
+                    }
+                    shipsPlaced[i] = true;
+                    for (int j = 0; j < count; j++)
+                    {
+                        dgvPlace.SelectedCells[j].Style.BackColor = Color.Red;
+                    }
+                    PlayGoodSound();
+                    return;
+                } else if (count == 3) { // Edge Case
+                    if (shipsPlaced[2] != true)
+                    {
+                        shipsPlaced[2] = true;
+                        for (int j = 0; j < count; j++)
+                        {
+                            dgvPlace.SelectedCells[j].Style.BackColor = Color.Red;
+                        }
+                        PlayGoodSound();
+                        return;
+                    }
+                    if (shipsPlaced[3] != true)
+                    {
+                        shipsPlaced[3] = true;
+                        for (int j = 0; j < count; j++)
+                        {
+                            dgvPlace.SelectedCells[j].Style.BackColor = Color.Red;
+                        }
+                        PlayGoodSound();
+                        return;
+                    }
+                    Interaction.MsgBox("You already placed a " + shipNames[2] + "/" + shipNames[3]);
+                    return;
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < shipsPlaced.Length; i++)
+            {
+                shipsPlaced[i] = false;
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    dgvPlace[i,j].Style.BackColor = Color.White;
+                }
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnPlaceShip.Enabled = false;
+            btnClear.Enabled = false;
         }
     }
 }
